6b186312a2110d51b1978bb2e9ba055d863d8b0f
James Robinson <admin@robinjam.net>
2013-11-10 20:14:12 +0000

    Switch to OpenGL 4.1

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int main(int argc, const char *argv[])
 		if (!glfwInit())
 			throw std::runtime_error("glfwInit failed");
 
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
